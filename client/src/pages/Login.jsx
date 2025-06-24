9f87a8a8831787c9826a48c8107ba38200582e1e
Try Indra Hatmojo <tryindra3375@gmail.com>
2025-06-24 20:59:08 +0700

    refactor: remove login form and replace with Google OAuth

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -28,39 +28,13 @@ export default function Login() {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl font-bold mb-4 text-center">
         Login to ModalNekat <br />
-        Your Business Planner
+        Your Business Planner App
       </h1>
       <p>Turn ideas into ready business proposals</p>
-      <form className="p-6 rounded shadow-md w-120">
-        <div className="mb-4">
-          <label className="block text-sm font-medium mb-2" htmlFor="email">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="w-full px-3 py-2 border rounded"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-medium mb-2" htmlFor="password">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="w-full px-3 py-2 border rounded"
-            required
-          />
-        </div>
-        <button type="submit" className="w-full btn btn-primary">
-          Login
-        </button>
-        <div className="rounded-selector flex items-center justify-center mt-4 bg-white p-1 ">
-          <div id="buttonDiv"></div>
-        </div>
-      </form>
+
+      <div className="rounded-selector flex items-center justify-center mt-4 bg-white p-1 w-63">
+        <div id="buttonDiv"></div>
+      </div>
     </div>
   );
 }
