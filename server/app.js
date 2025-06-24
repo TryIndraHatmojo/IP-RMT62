8207d97cf4f997d8a12f2c47911dbcd02a47bb72
Try Indra Hatmojo <tryindra3375@gmail.com>
2025-06-24 20:54:50 +0700

    feat: add POST /login/google for login and signup use Google OAuth

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+const cors = require("cors");
 const express = require("express");
+const LoginController = require("./controllers/LoginController");
+const errorHandler = require("./middlewares/errorHandler");
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.post("/login/google", LoginController.googleLogin);
+
+app.use(errorHandler);
 
 module.exports = app;
